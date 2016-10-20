0a88a455197ee0b45272d3d511772f603244881f
Joseph Egglestone <jegglestone@nhs.net>
2016-10-20 15:00:37 +0100

    Tests re-added

---
+++ Vendor.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,82 @@
+﻿using System.Web.Mvc;
+using Vendor.Controllers;
+
+namespace Vendor.Tests.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using NUnit.Framework;
+    using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [Test]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void BuyProduct_RedirectsToThankYouView()
+        {
+            HomeController controller = new HomeController();
+
+            ViewResult result = controller.BuyProduct(null, null) as ViewResult;
+
+            if (result != null)
+                Assert.AreEqual(result.ViewName, "ThankYou");
+        }
+
+        [TestCase(new double[] { 1 }, "Cola", "Thank you!", TestName = "WhenCorrectChange_Retursn_ThankYou")]
+        [TestCase(new double[] { 1 }, "Cola", "Thank you!", TestName = "WhenCorrectChangeMultipleCoins_Returns_ThankYou")]
+        [TestCase(new double[] { 0.5, 0.1, 0.05 }, "Candy", "Thank you!", TestName = "WhenCorrectChange_Returns_ThankYou")]
+        [TestCase(new double[]{0.5,0.1}, "Cola", "INSERT COINS", TestName = "WhenTooLittleChange_Returns_INSERTCOIN")]
+        [TestCase(new double[] { 0.5, 1 }, "Chips", "Thank you. Your change is £1", TestName = "WhenTooMuchChange_Returns_CorrectChange")]
+        [TestCase(new double[] { 1, 0.5, 1 }, "Chips", "Thank you. Your change is £2", TestName = "WhenTooMuchChange_Returns_CorrectChange")]
+        public void BuyProduct_Returns_ThankYou_when_correctChangeIsGiven(double[] change, string product, string expectedMessage)
+        {
+            HomeController controller = new HomeController();
+
+            var actualChange = change.ToList();
+
+            ViewResult result = controller.BuyProduct(actualChange, product) as ViewResult;
+
+            if (result != null)
+                Assert.AreEqual(result.ViewBag.Message, expectedMessage);
+        }
+    }
+}
---
+++ Vendor.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Vendor.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NHS Choices")]
+[assembly: AssemblyProduct("Vendor.Tests")]
+[assembly: AssemblyCopyright("Copyright © NHS Choices 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("efaff376-4e4d-4026-b244-6c393ca43cb4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
